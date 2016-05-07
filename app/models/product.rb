92af22b4a67185afa72c446938ca41b2b5d12699
Oliver Futureworkz <oliver@futureworkz.com>
2016-05-07 10:27:26 +0700

    Add Product#find_by_brands, #find_by_min_price, #find_by_max_price + rspec

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,4 +12,16 @@ class Product < ActiveRecord::Base
   validates_attachment :picture,
     content_type: {content_type: /\Aimage\/.*\Z/},
     size: { in: 0..5.megabytes, message: 'Photo size too large. Please limit to 5 mbs.' }
+
+  def self.find_by_brands(brands)
+    where(brand: brands)
+  end
+
+  def self.find_by_min_price(min_price)
+    where("price >= ?", min_price)
+  end
+
+  def self.find_by_max_price(max_price)
+    where("price <= ?", max_price)
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -20,4 +20,44 @@ RSpec.describe Product, type: :model do
         .less_than(5.megabytes)
     end
   end
+
+  describe '#find_by_brands' do
+    let!(:nike)         { create(:brand, name: 'Nike') }
+    let!(:ruby)         { create(:brand, name: 'Ruby') }
+    let!(:eyewear)      { create(:product, name: 'SAFETY EYEWEAR', brand: nike) }
+    let!(:rails)        { create(:product, name: 'ROR', brand: ruby) }
+    let!(:products)      { create_list(:product, 3)}
+
+    it 'return result' do
+      result = Product.find_by_brands([nike.id, ruby.id])
+
+      expect(result.size).to eq 2
+      expect(result).to include eyewear
+      expect(result).to include rails
+    end
+  end
+
+  describe "#find_by_min_price" do
+    let!(:ruby_product)  { create(:product, name: 'Ruby product', price: 200) }
+    let!(:products)      { create_list(:product, 3, price: 100) }
+
+    it "return result" do
+      result = Product.find_by_min_price(200)
+
+      expect(result.size).to eq 1
+      expect(result).to include ruby_product
+    end
+  end
+
+  describe "#find_by_max_price" do
+    let!(:ruby_product)  { create(:product, name: 'Ruby product', price: 100) }
+    let!(:products)      { create_list(:product, 3, price: 500) }
+
+    it "return result" do
+      result = Product.find_by_max_price(200)
+
+      expect(result.size).to eq 1
+      expect(result).to include ruby_product
+    end
+  end
 end
